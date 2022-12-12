ba4bbed4e8c85b907c009fbb8a43a53ef879f37a
john katua <johnkatua99@gmail.com>
2022-12-12 14:03:33 +0300

    added content to the intro carousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -20,12 +20,38 @@ function IntroCarousel() {
           <div className="intro--carousel__left">
             <div className='intro--left__one'>
               <h2>Earn with zero hustle</h2>
+              <p>
+                Our team of professional traders will help you secure a
+                Funded account and receive up to 90% of profits, 
+                while we do all the hard work for you.
+              </p>
+              <button>
+                Get Started
+              </button>
             </div>
-            <div className='test-two'>2</div>
+            <div className='intro--left__two'>
+              <img
+                src="assets/slide1.png"
+                alt="First slide"
+              /> 
+            </div>
           </div>
-          <div className="intro--carousel__right">
-            <div>!</div>
-            <div>2</div>
+          <div className="intro--carousel__left">
+            <div className='intro--left__one'>
+              <h2>Want to become A Funded Trader</h2>
+              <p>
+                We will help you PASS prop firm challenges and get a Funded Account.
+              </p>
+              <button>
+                Get Started
+              </button>
+            </div>
+            <div className='intro--left__two'>
+              <img
+                src="http://acepropfirmtraders.com/img/slide2.png"
+                alt="First slide"
+              /> 
+            </div>
           </div>
         </Slider>
     </div>
@@ -34,21 +60,21 @@ function IntroCarousel() {
     //     <div className='carousel--details'>
     //       <div className='intro--details__left'>
     //         <h2>Earn with Zero hustle</h2>
-    //         <p>
-    //           Our team of professional traders will help you secure a
-    //           Funded account and receive up to 90% of profits, 
-    //           while we do all the hard work for you.
-    //         </p>
-    //         <button>
-    //           Get Started
-    //         </button>
+            // <p>
+            //   Our team of professional traders will help you secure a
+            //   Funded account and receive up to 90% of profits, 
+            //   while we do all the hard work for you.
+            // </p>
+            // <button>
+            //   Get Started
+            // </button>
     //       </div>
     //       <div className='intro--details__right'>
-    //         <img
-    //           // className='h-100'
-    //           src="assets/slide1.png"
-    //           alt="First slide"
-    //         /> 
+            // <img
+            //   // className='h-100'
+            //   src="assets/slide1.png"
+            //   alt="First slide"
+            // /> 
     //       </div>
     //     </div>
     //   </Carousel.Item>
