4bc00099cdc6836d50523c365a6c213adb31a83d
john katua <johnkatua99@gmail.com>
2022-12-12 20:17:06 +0300

    installed react select

--- src/components/challengeOverlay.jsx/ChallengeOverlay.jsx
+++ src/components/challengeOverlay.jsx/ChallengeOverlay.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './challengeOverlay.css';
+import Select from 'react-select';
+
+const options = [
+  { value: 'usd', label: 'USD' },
+  { value: 'kes', label: 'KES' },
+  { value: 'aed', label: 'AED' },
+  { value: 'cad',  label: 'CAD' },
+  { value: 'eur', label: 'EUR' }
+]
 
 const ChallengeOverlay = ({ closeModal }) => {
+  const [selectedOption, setSelectedOption] = useState(null);
   return (
-    <div onClick={closeModal}>ChallengeOverlay</div>
+    <div className='challenge--overlay'>
+      <div className="challenge--overlay__item">
+        <div className='challnge--overlay__firstName'>
+          <label>First Name:</label>
+          <span onClick={closeModal}>X</span>
+        </div>
+        <input type="text" />
+      </div>
+      <div className="challenge--overlay__item">
+        <label>Last Name:</label>
+        <input type="text" />
+      </div>
+      <div className="challenge--overlay__item">
+        <label>Email:</label>
+        <input type="email" />
+      </div>
+      <div className='challenge--overlay__currency'>
+        <div>Currency</div>
+        <Select 
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
+          options={options}
+        />
+      </div>
+      <button>Submit</button>
+    </div>
   )
 }
 
