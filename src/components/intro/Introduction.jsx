a6802a3e7ee106cd97bbeea7e38ea0b73f284645
john katua <johnkatua99@gmail.com>
2022-12-12 17:09:50 +0300

    removed unused code

--- src/components/intro/Introduction.jsx
+++ src/components/intro/Introduction.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import './intro.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import IntroCarousel from '../carousel/Carousel';
 
 const Introduction = () => {
   return (
-    <div className='intro--container'>
+    <div className='intro--container' id='intro'>
       <div className='intro--image'>
       </div>
       <div className='intro--details'>
         <IntroCarousel />
       </div>
-      {/* <div className='intro--details'>
-        <IntroCarousel />
-      </div> */}
     </div>
   )
 }
